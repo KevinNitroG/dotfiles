1e5454aa7aeade3544aa6f43e2d420c0873ce4bd
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-06 22:43:48 +0700

    fix(windows): enable dev mode

--- home/.chezmoiscripts/windows/run_once_before_10_install-choco.ps1
+++ home/.chezmoiscripts/windows/run_once_before_10_install-choco.ps1
@@ -5,6 +5,5 @@ if (Get-Command 'choco')
 
 Write-Host 'INSTALLING CHOCO...'
 
-Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
--- home/.chezmoiscripts/windows/run_once_before_10_install-scoop.ps1
+++ home/.chezmoiscripts/windows/run_once_before_10_install-scoop.ps1
@@ -5,6 +5,4 @@ if (Get-Command 'scoop')
 
 Write-Host 'INSTALLING SCOOP...'
 
-Import-Module Microsoft.PowerShell.Security
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
