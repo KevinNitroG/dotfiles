60b0fb29675fafdfaa95765800e43981767a0beb
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-13 20:35:56 +0700

    refactor: move tmux config to .config folder

--- home/.chezmoiscripts/linux/run_once_before_100-setup-tmux-resurrect-systemd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v tmux &>/dev/null || [ ! -f "$HOME/.config/systemd/user/tmux.service" ]; then
-  exit
-fi
-
-echo 'SETTING UP TMUX RESURRECT SYSTEMD...'
-systemctl --user enable tmux.service
