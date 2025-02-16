2944a2447c5c6deb2c41a07395c5831cc94ea508
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-16 14:26:01 +0700

    feat: clean init

---
+++ home/.chezmoiscripts/windows/run_once_before_10_install-choco.ps1
@@ -0,0 +1,9 @@
+if (Get-Command 'choco')
+{
+  exit
+}
+
+Write-Host 'INSTALLING CHOCO...'
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
