dd3390eaadad17b713aee5c6e62ed8d498f4521d
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-04-27 22:14:46 +0700

    fix(hyde): install new HyDE

--- home/.chezmoiscripts/linux/run_once_before_50-install-hyde.sh
+++ home/.chezmoiscripts/linux/run_once_before_50-install-hyde.sh
@@ -6,5 +6,5 @@ if [ -d "$HOME/HyDE" ]; then
   exit 0
 fi
 
-git clone --depth 1 https://github.com/prasanthrangan/hyprdots ~/HyDE
+git clone --depth 1 https://github.com/HyDE-Project/HyDE.git ~/HyDE
 ~/HyDE/Scripts/Install.sh
